bd39948a34d4db5a0e57bf61aed13bfaf423e690
Eslam Saadat (Iman) <80543205+eslamsaadat@users.noreply.github.com>
2023-04-23 19:31:21 +0430

    Navbar was updated

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,15 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 import Logo from "../../assets/logo.png";
 import { NavLinksData } from "../../data/data";
 import NavLinks from "./NavLinks";
 const Navbar = () => {
   const [navbar, setNavbar] = useState(false);
+  const [scroll, setScroll] = useState(false);
   const handleNavbar = () => {
     setNavbar(!navbar);
   };
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setScroll(true);
+      } else {
+        setScroll(false);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+  }, []);
   return (
-    <header className="w-full fixed left-0 right-0 top-0">
+    <header
+      className={`w-full fixed left-0 right-0 top-0 z-30 ${
+        scroll ? "backdrop-blur-md bg-primaryColor/60" : ""
+      }`}
+    >
       <nav className="flex justify-between items-center max-w-screen-xl mx-auto px-4 md:px-8 py-2">
         <div className="z-30">
           <img src={Logo} className="w-14 h-14" alt="eslam saadat" />
